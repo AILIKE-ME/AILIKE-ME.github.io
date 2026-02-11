d7a7334ba04754583c85f11b365543e5d1b5e32e
AILIKE-ME <hrpark@ailike.me>
2026-02-11 23:10:13 +0900

    add liquidation notice popup

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,15 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
+    alert(
+        '해 산 및 채 권 신 고 공 고(1차)\n\n' +
+        '본 회사는 2026 년 1 월 10 일 임시주주총회에서 해산결의되었으므로 본 회사에 대하여 채권이 있는 분은 공고일 익일부터 2개월 이내에 그 채권액을 본 회사에 신고하여 주시기 바라며, 만일 위 기일내에 신고가 없으면 청산에서 제외됩니다.\n\n' +
+        '2026 년 2 월 12 일\n\n' +
+        '주식회사 에이아이라이크미\n' +
+        '인천광역시 미추홀구 아암대로 109, 3층 304호,305호-40(용현동)\n' +
+        '청산인 박 하 림'
+    );
+
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
